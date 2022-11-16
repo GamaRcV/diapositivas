a9356cc0457a6d624042d185611645f9857f24c1
GamaRcV <110438108+GamaRcV@users.noreply.github.com>
2022-11-16 11:19:16 -0600

    corriegiendo nombre de unidades

--- pages/temario.js
+++ pages/temario.js
@@ -13,7 +13,7 @@ export default function Temario() {
         <h1 className='heading'>Temario</h1>
         <div className={styles.grid}>
           <div className={styles.equipo}>
-            <h2>Unidad 1</h2>
+            <h2>Introducción</h2>
             <Link href='/informacion'>
               <a title='Hongos macroscopicos y microscópicos'>
                 <Image src='/img/hongos.jpg' width={335.61} height={188.7} alt='Hongos macroscopicos y microscópicos ' />
@@ -22,7 +22,7 @@ export default function Temario() {
           </div>
 
           <div className={styles.equipo}>
-            <h2>Unidad 2</h2>
+            <h2>Antimicoticos</h2>
             
             <Link href="https://onedrive.live.com/embed?cid=14BF5B6A7B6AEDEF&amp;resid=14BF5B6A7B6AEDEF%21129&amp;authkey=AJkUhco8GP_qCd4&amp;em=2&amp;wdAr=1.7777777777777777" width="476px" height="288px">
               <a title='Antimicóticos'>
@@ -34,7 +34,7 @@ export default function Temario() {
 
           <div className={styles.equipo}>
             <div>
-            <h2>Unidad 3</h2>
+            <h2>Micosis superficiales...</h2>
             <Link href="https://onedrive.live.com/embed?cid=14BF5B6A7B6AEDEF&amp;resid=14BF5B6A7B6AEDEF%21131&amp;authkey=AP3GTUvZiP7o-FU&amp;em=2&amp;wdAr=1.7777777777777777" width="476px" height="288px">
               <a title='Micosis superficiales y subcutáneas'>
                 <Image src='/img/micosissuperficiales.png' width={335.61} height={188.7} alt='Micosis superficiales y subcutaneas'/>
@@ -44,7 +44,7 @@ export default function Temario() {
           </div>
 
           <div className={styles.equipo}>
-            <h2>Unidad 4</h2>
+            <h2>Micosis Sistémicas</h2>
             <Link href="https://onedrive.live.com/embed?cid=14BF5B6A7B6AEDEF&amp;resid=14BF5B6A7B6AEDEF%21124&amp;authkey=ABh0wU2WHk3X8dU&amp;em=2&amp;wdAr=1.33203125" width="476px" height="288px">
               <a title='Micosis Sistematica'>
                 <Image src='/img/micosis.png' width={335.61} height={188.7} alt='Micosis Sistematica' />
@@ -53,7 +53,7 @@ export default function Temario() {
           </div>
 
           <div className={styles.equipo}>
-            <h2>Unidad 5</h2>
+            <h2>Micosis oportunistas...</h2>
             <Link href="https://onedrive.live.com/embed?cid=14BF5B6A7B6AEDEF&amp;resid=14BF5B6A7B6AEDEF%21133&amp;authkey=AJA5C85qhtKxXbo&amp;em=2&amp;wdAr=1.7777777777777777" width="476px" height="288px">
               <a title='Micosis oportunistas y pseudomicosis'>
                 <Image src='/img/pseudomicosis.png' width={335.61} height={188.7} alt='Micosis oportunistas y pseudomicosis' />
@@ -62,7 +62,7 @@ export default function Temario() {
           </div>
 
           <div className={styles.equipo}>
-            <h2>Unidad 6</h2>
+            <h2>Otras patologías</h2>
             <Link href="https://onedrive.live.com/embed?cid=14BF5B6A7B6AEDEF&amp;resid=14BF5B6A7B6AEDEF%21135&amp;authkey=ADEVT2OqPJFBGHE&amp;em=2&amp;wdAr=1.7777777777777777" width="476px" height="288px">
               <a title='Otras patologías( nicetismo, micotoxicosis)'>
                 <Image src='/img/micetismo.webp' width={335.61} height={188.7} alt='Otras patologías( nicetismo, micotoxicosis)' />
